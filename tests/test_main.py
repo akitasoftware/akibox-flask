9706635232a2e952576f1a3fd4fdacbb4b7937bc
Cole Schlesinger <cole@akitasoftware.com>
2021-02-11 15:05:04 -0800

    Switch to Apache License 2.0

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Akita Software, Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import copy
 import json
 import pytest
@@ -10,7 +24,7 @@ def client():
     app.config['TESTING'] = True
     app.test_client_class = HarClient
 
-    with app.test_client() as client:
+    with app.test_client(har_file_path="my_trace.har") as client:
         yield client
 
 def test_default_users(client):
